174652bf00740414263f95a196eabfea5f6ac67f
ByatisGlaaki <rares_famuntean@yahoo.com>
2025-04-17 20:55:05 +0300

    proper fixes for the drop function

--- src/Components/UploadImageArea/UploadImageArea.tsx
+++ src/Components/UploadImageArea/UploadImageArea.tsx
@@ -1,52 +1,56 @@
-import { motion } from "motion/react";
+import { motion } from "framer-motion"; // Make sure you import from 'framer-motion'
 import { LuImageUp } from "react-icons/lu";
 import { basicOpacityAnimation } from "../../styling/constants";
-import Dropzone from "react-dropzone";
+import { FileRejection, useDropzone } from "react-dropzone";
 import "./UploadImageArea.scss";
+import { useEffect, useState } from "react";
 
 const UploadImageArea = () => {
-    return (
-        <Dropzone
-            onDrop={(acceptedFiles, fileRejections) => {
-                if (fileRejections.length > 0) {
-                    console.log(fileRejections[0]["file"]);
-                } else {
-                    console.log(acceptedFiles);
-                }
-            }}
-            noClick
-            noKeyboard
-            multiple={false}
-        >
-            {({ getRootProps, getInputProps, open }) => (
-                <div {...getRootProps()} className="upload-area">
-                    <motion.div
-                        initial={{ opacity: 0, y: 100 }}
-                        animate={{ opacity: 1, y: 0 }}
-                    >
-                        <LuImageUp className="icon-img" />
-                    </motion.div>
+    const [uploadError, setUploadError] = useState("");
 
-                    <motion.p
-                        variants={basicOpacityAnimation}
-                        initial="initial"
-                        animate="animate"
-                    >
-                        Drag and Drop Your Image Here
-                    </motion.p>
-                    <motion.p
-                        variants={basicOpacityAnimation}
-                        initial="initial"
-                        animate="animate"
-                        className="select-image-text"
-                    >
-                        or <span onClick={open}>select an image</span> from your
-                        computer
-                    </motion.p>
-                    <input {...getInputProps()} />
-                </div>
-            )}
-        </Dropzone>
+    const onDrop = (acceptedFiles: File[], fileRejections: FileRejection[]) => {
+        if (fileRejections.length > 0) {
+            setUploadError("Invalid file type!");
+        } else if (acceptedFiles.length > 0) {
+            console.log("Accepted file: ", acceptedFiles[0]);
+        }
+    };
+
+    const { getRootProps, getInputProps, open } = useDropzone({
+        accept: { "image/*": [] },
+        multiple: true,
+        noClick: true,
+        noKeyboard: true,
+        onDrop,
+    });
+
+    return (
+        <div {...getRootProps()} className="upload-area">
+            <motion.div
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+            >
+                <LuImageUp className="icon-img" />
+            </motion.div>
+
+            <motion.p
+                variants={basicOpacityAnimation}
+                initial="initial"
+                animate="animate"
+            >
+                Drag and Drop Your Image Here
+            </motion.p>
+            <motion.p
+                variants={basicOpacityAnimation}
+                initial="initial"
+                animate="animate"
+                className="select-image-text"
+            >
+                or <span onClick={open}>select an image</span> from your
+                computer
+            </motion.p>
+            <input {...getInputProps()} multiple={false} />
+        </div>
     );
 };
 
